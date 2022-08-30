8ffc31b682728c08ae08d8fbd10c40a7216c55cc
zenon009 <zenon.lambrou@gmail.com>
2022-08-30 11:05:47 +0300

    Cleaning scripts

--- scripts/albert/features/normal/extract_features.sh
+++
@@ -1,1 +0,0 @@
-python D:/pycharmProjects/Brain-Transformer-Mapping-On-Punctuation/brain_language_nlp/extract_nlp_features.py --nlp_model albert --sequence_length 4 --output_dir D:/pycharmProjects/Brain-Transformer-Mapping-On-Punctuation/data/models_output/albert/features/normal/4/ --home_path D:/pycharmProjects/Brain-Transformer-Mapping-On-Punctuation --feature_strategy normal
